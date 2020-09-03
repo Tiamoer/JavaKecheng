4eddd558166200a37d49ee45a98c04490e4f4d2f
TiAmo <tiamoer@outlook.com>
2020-09-03 08:12:30 +0800

    ADD FILE

---
+++ src/chapter1/BasicDataType.java
@@ -0,0 +1,9 @@
+package chapter1;
+// 基本数据类型
+public class BasicDataType {
+    public static void main(String[] args) {
+    //    Java语言中的Boolean定义的关键字是Boolean
+    //    真值是true 假值是false
+    //    关键字都是小写
+    }
+}
