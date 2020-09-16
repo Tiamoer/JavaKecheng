d24293e3167a0340ad91852270e794a3e95fc436
TiAmo <tiamoer@outlook.com>
2020-09-17 07:43:04 +0800

    demo1

---
+++ src/demo/MathMenu.java
@@ -0,0 +1,54 @@
+package demo;
+
+public class MathMenu {
+    public static void main(String[] args) {
+        String name = new A("张三",18,"Gansu","15193995886").getName();
+        System.out.println(name);
+    }
+}
+
+class A {
+    private String name;
+    private int age;
+    private String address;
+    private String phone;
+
+    public A(String name, int age, String address, String phone) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+}
