35773f17618a6a5c56151d280234f3dab37f8536
Tiamoer <tiamoer@outlook.com>
2020-11-26 08:21:31 +0800

    JDBC

--- src/JDBC/Java_Mysql8.java
+++ src/JDBC/Java_Mysql8.java
@@ -8,14 +8,31 @@ public class Java_Mysql8 {
     static final String user = "root";
     static final String pass = "745700yxy";
 
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, InterruptedException {
         Class.forName("com.mysql.jdbc.Driver");
+        System.out.println("驱动加载完成...");
+        Thread.sleep(1000);
         System.out.println("连接数据库...");
         Connection connection = DriverManager.getConnection(url,user,pass);
+        Thread.sleep(1000);
+        System.out.println("建立查询...");
         Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery("select * from stu");
+        String sql = "select * from stu";
+        ResultSet resultSet = statement.executeQuery(sql);
         while (resultSet.next()) {
-            System.out.println(resultSet.getString("Sname"));
+            System.out.print("Sno: "+resultSet.getString("Sno")+"\t");
+            System.out.print("Sname: "+resultSet.getString("Sname")+"\t");
+            System.out.print("Ssex: "+resultSet.getString("Ssex")+"\t");
+            System.out.print("Sdept: "+resultSet.getString("Sdept")+"\t");
+            System.out.print("Szzmm: "+resultSet.getString("Szzmm")+"\n");
+        }
+        System.out.println("查询结束！");
+        try {
+            if (resultSet!=null) resultSet.close();
+            if (statement!=null) statement.close();
+            if (connection!=null) connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
